577ce079cbcbf18470f0e16a26edb46517255bc2
Isaiah Gonzales <isaiahg46@gmail.com>
2025-03-27 18:32:40 -0700

    added jwt code to app.py

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",   // Add your HTML file(s)
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/TS/React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
